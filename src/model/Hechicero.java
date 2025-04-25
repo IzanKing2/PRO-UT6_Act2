5a95694bbbd8d1bec6e0bece8a14ea41cb9ba9eb
IzanKing2 <izanwork2@gmail.com>
2025-04-25 12:53:25 +0100

    Actualizar clase Hechicero: ajustar constantes de costo de hechizo y defensa, y corregir la inicialización de concentración en el constructor.

--- src/model/Hechicero.java
+++ src/model/Hechicero.java
@@ -6,14 +6,15 @@ import model.interfaces.*;
 // 🧙‍♂️
 public class Hechicero extends Magica implements Magico, Defendible {
     // Constantes _______________
-    private static final int COSTO_HECHIZO = 5;
-    private static final int REGENERACION_MANA = 30;
-    private static final int DEFENSA = 50;
+    private static final int COSTO_HECHIZO = 8;
+    private static final int DEFENSA = 20;
+    private static final int COSTO_DEFENSA = 40;
 
     // Atributos ______________
     private int maxMana = 80;
     private int maxSalud = 40;
     private int maxDaño = 15;
+    private int maxConcentracion = 90;
     private int mana;
     private int concentracion;
 
@@ -21,25 +22,28 @@ public class Hechicero extends Magica implements Magico, Defendible {
     public Hechicero(String nombre) {
         super(nombre, 1, 40, 20); // Salud inicial
         this.mana = maxMana;
-        this.concentracion = 90;
+        this.concentracion = maxConcentracion;
     }
 
     // Métodos _____________________________
-    @Override
-    public void lanzarHechizo() {
-        System.out.println("✨ Lanzando hechizo...");
-        if (this.mana < COSTO_HECHIZO) {
-            System.out.println("❌ No tienes suficiente mana para lanzar un hechizo.");
-        } else {
-            this.mana -= COSTO_HECHIZO;
-            System.out.println("✅ Hechizo lanzado con éxito. Mana restante: " + this.mana);
-        }
-    }
+    
+    
 
+    /**
+     *  Se defiende, reduciendo la concentración del hechicero.
+     *  Si no tiene suficiente concentración, no puede defenderse.
+     *  Ya que la defensa es mágica, se suma a la salud del hechicero.
+     */
     @Override
     public void defender() {
-        // TODO Auto-generated method stub
-        
+        System.out.println("🛡️ Defendiendo con magia...");
+        if (this.concentracion < COSTO_DEFENSA) {
+            System.out.println("❌ No tienes suficiente concentración");
+        } else {
+            this.concentracion -= COSTO_DEFENSA;
+            System.out.println("DEFENSA aplicada con exito");
+            this.setSalud(this.getSalud() + DEFENSA);
+        }
     }
 
     // Getters y Setters _________________
