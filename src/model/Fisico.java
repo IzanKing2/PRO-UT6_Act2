5cc4df8c45aa0e47fc8dbfad75293bb1e5b14611
Saulinho55 <Saulespino555@gmail.com>
2025-05-02 17:05:41 +0100

    Refactor clase Guerrero: agregar atributos de fuerza, armadura y escudo; implementar métodos para cargar ataque y defenderse.

--- src/model/Fisico.java
+++ src/model/Fisico.java
@@ -38,8 +38,5 @@ public class Fisico extends Personaje {
         super.setNivel(super.getNivel() + 1);
         System.out.println("🎉 ¡Has subido de nivel! Nivel actual: " + super.getNivel());
     }
-
-    
-    
 }
 
