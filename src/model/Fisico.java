60787d01702fb89633e081612fcec599d8c99db8
Diego12132025 <diegowork2004@gmail.com>
2025-05-02 14:12:55 +0100

    Implementar métodos de combate y nivelación en la clase Fisico; actualizar la clase Guerrero para heredar de Fisico e implementar la interfaz Defendible.

--- src/model/Fisico.java
+++ src/model/Fisico.java
@@ -2,8 +2,44 @@ package model;
 
 // Clase Fisico que hereda de Personaje y representa un personaje físico en el juego
 // 🏋️‍♂️
-/**
- * public class Fisico extends Personaje {
+
+public class Fisico extends Personaje {
+
+    // Constructor ______________________________________________________
+    public Fisico(String nombre, int nivel, int salud, int daño) {
+        super(nombre, nivel, salud, daño);
+    }
+
+    // Métodos __________________________________________________________
+    /**
+     *  Método para atacar a otro personaje
+     *  @param enemigo el personaje enemigo al que se va a atacar
+     */
+    @Override
+    public void realizarAccion(int opcion, Personaje enemigo) {
+        System.out.println("Realizando acción física...");
+        // Lógica para realizar una acción física
+    }
+
+    @Override
+    public void atacar(Personaje enemigo) {
+        int daño = super.getDaño();
+        enemigo.setSalud(enemigo.getSalud() - daño);
+        System.out.println(" Atacaste a " + enemigo.getNombre() + " e infligiste " + daño + " puntos de daño.");
+    }
+
+    @Override
+    public void menuCombate() {
+        System.out.println("__MENÚ DE COMBATE MÁGICO__");
+    }
+
+    @Override
+    public void subirNivel() {
+        super.setNivel(super.getNivel() + 1);
+        System.out.println("🎉 ¡Has subido de nivel! Nivel actual: " + super.getNivel());
+    }
+
+    
     
 }
- */
+
--- src/model/Guerrero.java
+++ src/model/Guerrero.java
@@ -1,9 +1,14 @@
 package model;
 
+import model.interfaces.Defendible;
+
 // Clase Guerrero que hereda de la clase Fisico y representa un personaje guerrero en el juego
 // 🛡️
-/**
-public class Guerrero extends Fisico {
+
+/* 
+public class Guerrero extends Fisico implements Defendible {
+    // Constantes ____________________________________
+
     
 }
 */
